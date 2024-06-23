d8eb077cd085118991562c044f6e714c04912a15
cyd dalupan <cydmdalupan@gmail.com>
2024-06-23 04:51:23 +0000

    update settings

--- expatcare/urls.py
+++ expatcare/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('playground/', include('playground.urls')),
+    path('assistant/', include('assistant.urls')),
 ]

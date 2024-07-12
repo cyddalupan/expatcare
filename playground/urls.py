0e9450425a42aa34593c9a9580262ad2f33c88bc
cyd dalupan <cydmdalupan@gmail.com>
2024-07-12 05:39:43 +0000

    Add playground for assistant

--- playground/urls.py
+++ playground/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet
+from .views import ActionsGPTWebhook, HelloWorld, TaskViewSet
 
 router = DefaultRouter()
 router.register(r'Task', TaskViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('hello/', HelloWorld.as_view(), name='hello-world'),
+    path('actions-gpt-webhook/', ActionsGPTWebhook.as_view(), name='actions-gpt-webhook'),
 ]

1bbb0ec2643e7daa11ae5736049c4a47644332ba
Cleddrence Dalupan <cydmdalupan@gmail.com>
2024-07-12 11:56:39 +0000

    fix cross access

--- expatcare/settings.py
+++ expatcare/settings.py
@@ -162,12 +162,16 @@ ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 # }
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-    ],
+   # 'DEFAULT_AUTHENTICATION_CLASSES': [
+   #     'rest_framework.authentication.SessionAuthentication',
+   #     'rest_framework.authentication.TokenAuthentication',
+   # ],
+   # 'DEFAULT_PERMISSION_CLASSES': [
+   #     'rest_framework.permissions.IsAuthenticated',
+    #],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
 }
 

db92338f8c986a7fa9e1b17c1d62defa27fcf265
Cleddrence Dalupan <cydmdalupan@gmail.com>
2024-07-20 16:16:20 +0800

    Incomplete Backend

--- cases/urls.py
+++ cases/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CaseViewSet
-from .views import Chat
+from cases.chat_view import Chat
+from cases.other_view import Other
+from cases.report_view import Report
 
 router = DefaultRouter()
-router.register(r'cases', CaseViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('other/', Other.as_view(), name='other'),
     path('chat/', Chat.as_view(), name='chat'),
+    path('report/', Report.as_view(), name='report'),
 ]

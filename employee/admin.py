4a6a91c978f355bb87e6b8fa474ab06dc207af18
Cleddrence Dalupan <cydmdalupan@gmail.com>
2024-08-04 14:29:01 +0800

    add agency to applicants

--- employee/admin.py
+++ employee/admin.py
@@ -11,15 +11,19 @@ class EmployeeAdmin(admin.ModelAdmin):
         'address', 
         'phone_number', 
         'email', 
-        'emergency_contact_name', 
-        'emergency_contact_phone'
+        'agency', 
+        'emergency_contact_name',
     )
     search_fields = (
         'first_name', 
+        'middle_name', 
         'last_name', 
         'passport_number', 
         'phone_number', 
-        'email'
+        'email',
+        'agency__username', 
+        'address', 
+        'emergency_contact_name', 
     )
     list_filter = (
         'date_of_birth',
@@ -34,7 +38,7 @@ class EmployeeAdmin(admin.ModelAdmin):
                 'date_of_birth', 
                 'address', 
                 'phone_number', 
-                'email'
+                'email',
             )
         }),
         ('Emergency Contact', {
@@ -44,5 +48,22 @@ class EmployeeAdmin(admin.ModelAdmin):
             )
         }),
     )
+
+    def agency_name(self, obj):
+        return obj.agency.username 
+    agency_name.admin_order_field = 'agency__username' 
+    agency_name.short_description = 'Agency Username' 
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.groups.filter(name='Agency').exists():
+            # Assuming that the 'agency' field is a ForeignKey to the User model
+            return qs.filter(agency=request.user)
+        return qs
+    def save_model(self, request, obj, form, change):
+        if not change or not obj.agency:
+            # Only set the agency if the object is new or doesn't have one
+            obj.agency = request.user
+        super().save_model(request, obj, form, change)
 
 admin.site.register(Employee, EmployeeAdmin)
--- employee/models.py
+++ employee/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=100)
@@ -11,6 +12,12 @@ class Employee(models.Model):
     email = models.EmailField()
     emergency_contact_name = models.CharField(max_length=100)
     emergency_contact_phone = models.CharField(max_length=20)
+    agency = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'groups__name': 'Agency'}, related_name='employees')
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+    
+    class Meta:
+        verbose_name = "Applicant"
+        verbose_name_plural = "Applicants"

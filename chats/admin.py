8c30f032329377c85e446382d63d492a09a2ca81
cyd dalupan <cydmdalupan@gmail.com>
2024-08-12 07:47:30 +0000

    Group chat per user

--- chats/admin.py
+++ chats/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
+from employee.models import Employee
 from .models import Chat
 
-@admin.register(Chat)
-class ChatAdmin(admin.ModelAdmin):
-    list_display = ('employee', 'timestamp')
-    #list_filter = ('employee', 'agency', 'sender')
-    #search_fields = ('employee__first_name', 'employee__last_name', 'agency__username', 'message')
+admin.site.unregister(Employee)
+
+class ChatInline(admin.TabularInline):
+    model = Chat
+    fields = ('message', 'sender', 'timestamp')
+    readonly_fields = ('message', 'sender', 'timestamp')
+    extra = 0
+    can_delete = False
+    show_change_link = False
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'passport_number', 'agency')
+    search_fields = ('first_name', 'last_name', 'passport_number', 'agency__username')
+    inlines = [ChatInline]
